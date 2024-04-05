1374fc5b36e0de46b39205ba4f16d4c13f5d0b65
Franklin Alvarado <frankivan.dev@gmail.com>
2024-04-04 19:37:19 -0500

    add authenticate validation

--- src/layouts/user.tsx
+++ src/layouts/user.tsx
@@ -1,13 +1,20 @@
 import { UserHeader } from '@components/user-header'
 import { Footer } from '@components/footer'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated'
 
 export function UserLayout() {
+  const isAuthenticated = useIsAuthenticated()
+
+  if (!isAuthenticated()) {
+    return <Navigate to='/' />
+  }
+
   return (
-    <main>
+    <>
       <UserHeader />
       <Outlet />
       <Footer />
-    </main>
+    </>
   )
 }
