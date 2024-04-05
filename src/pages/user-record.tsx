468e25d4bbbdc510dce5cc75fedf39b86fdaaaef
Franklin Alvarado <frankivan.dev@gmail.com>
2024-04-04 19:16:48 -0500

    feat user record page

--- src/pages/user-record.tsx
+++ src/pages/user-record.tsx
@@ -1,25 +1,77 @@
-import { GET } from '@src/api'
-import { UserCotizationResponse } from '@src/types'
 import { useEffect, useState } from 'react'
+import { GET } from '@src/api'
+import { Link } from 'react-router-dom'
+import { BasicTable } from '@components/table'
+import { BarListChart } from '@components/bar-list'
+import { Card } from '@tremor/react'
+import type { UserCotizationResponse } from '@src/types'
 import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader'
 
+const defaultState = {
+  numberOfQuotes: 0,
+  quotes: [],
+  top3Brands: []
+}
+
 export function UserRecordPage() {
-  const [recordInfo, setRecordInfo] = useState<UserCotizationResponse[]>([])
+  const [data, setData] = useState<UserCotizationResponse>(defaultState)
   const authHeader = useAuthHeader() as string
 
   useEffect(
     function () {
-      GET<{ cotizations: UserCotizationResponse[] }>('cotization/user', {
+      GET<UserCotizationResponse>('cotization/user', {
         Authorization: authHeader
-      }).then(res => setRecordInfo(res.cotizations))
+      }).then((res) => setData(res))
     },
     [authHeader]
   )
 
   return (
-    <>
-      <h1>Mis Cotizaciones</h1>
-      <p>Este es el historial de todas tus cotizaciones.</p>
-    </>
+    <section className='bg-red-0 h-auto relative pt-10 sm:min-h-dvh'>
+      <div className='bg-red-0 p-10 sm:max-w-screen-xl sm:mx-auto'>
+        <header className='bg-yellow-0 flex flex-col justify-between gap-2 py-4 mb-14 border-b sm:flex-row sm:items-center'>
+          <h1 className='text-gray-800 text-xl font-semibold sm:text-2xl'>
+            Record History
+          </h1>
+
+          <div className='flex items-center flex-wrap gap-2'>
+            <Link
+              to='/user'
+              className='bg-orange-500 text-gray-100 w-full text-center font-semibold text-xs px-6 py-2 rounded-lg hover:bg-orange-600 transition-colors sm:w-fit sm:text-sm'
+            >
+              + New Cotization
+            </Link>
+            <button className='bg-gray-100 text-gray-700 w-full text-center font-semibold text-xs px-6 py-2 border rounded-lg hover:bg-gray-200 transition-colors sm:w-fit sm:text-sm'>
+              Payment
+            </button>
+          </div>
+        </header>
+
+        <div className='bg-red-0 sm:max-w-4xl sm:mx-auto'>
+          <div className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
+            {/*  */}
+            <Card className=' flex flex-col items-center gap-2'>
+              <h3 className='text-base font-semibold sm:text-lg'>
+                Number of records
+              </h3>
+              <p className='text-orange-400 text-6xl'>{data.numberOfQuotes}</p>
+            </Card>
+
+            <BarListChart
+              data={data.top3Brands}
+              title='Cars with more Quotes'
+            />
+
+            <Card className='sm:col-span-2'>
+              <BasicTable
+                data={data.quotes}
+                title='List of quotes'
+              />
+            </Card>
+            {/*  */}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
