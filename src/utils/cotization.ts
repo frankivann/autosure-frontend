13616c98088ba3c9a80883ac650745508034623f
Franklin Alvarado <frankivan.dev@gmail.com>
2024-04-04 19:17:14 -0500

    feat has empty values utility

--- src/utils/cotization.ts
+++ src/utils/cotization.ts
@@ -2,7 +2,7 @@ import type { Cotization } from '@src/types'
 
 export function calculateCotizationCost (cotization: Cotization): string {
   const { year, numberOfDoors, location, usage, fuelType } = cotization
-  let cost = 700 // base cost
+  let cost = 500
 
   const carAge = new Date().getFullYear() - Number(year)
   cost -= carAge
@@ -11,13 +11,8 @@ export function calculateCotizationCost (cotization: Cotization): string {
     cost += 50 * (Number(numberOfDoors) - 2)
   }
 
-  if (location === 'Rural') {
-    cost += 125
-  }
-
-  if (usage === 'Commercial') {
-    cost += 220
-  }
+  location === 'Rural' ? cost += 122 : cost += 96
+  usage === 'Commercial' ? cost += 201 : cost += 88
 
   if (fuelType === 'Gasoline') cost += 194
   else if (fuelType === 'Diesel') cost += 65
@@ -26,4 +21,8 @@ export function calculateCotizationCost (cotization: Cotization): string {
 
   const totalCost = cost < 0 || cost > 5000 ? 0 : cost
   return totalCost.toString()
+}
+
+export function hasEmptyValues<T extends Record<string, unknown>>(obj: T): boolean {
+  return Object.values(obj).some(value => value === '')
 }
