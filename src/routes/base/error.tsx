006a2eef9889bae04ac9d876c2db17cc40bf5f59
frankivann <frankivan.dev@gmail.com>
2024-04-29 16:20:01 -0500

    add basic styles for error page

--- src/routes/base/error.tsx
+++ src/routes/base/error.tsx
@@ -1,5 +1,22 @@
-export function ErrorPage () {
+import { Link } from 'react-router-dom'
+
+export function ErrorPage() {
   return (
-    <h2>404</h2>
+    <section className='relative'>
+      <div className='bg-orange-50 min-h-dvh grid place-content-center'>
+        <h2 className='text-4xl text-center font-light mb-4 sm:text-7xl'>
+          404
+        </h2>
+        <p>
+          Not found |{' '}
+          <Link
+            to='/'
+            className='text-orange-500 hover:text-orange-600'
+          >
+            Go home
+          </Link>
+        </p>
+      </div>
+    </section>
   )
 }
